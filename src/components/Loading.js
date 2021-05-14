625e877e8cf4565b2d6a57a5908f4c5f28235e63
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-05-15 05:24:40 +0530

    first commit

---
+++ src/components/Loading.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { StyleSheet, Text} from 'react-native'
+
+import { View } from 'moti'
+import {RowView} from 'styles'
+import color from 'colors'
+
+const Loading = () => {
+    return (
+        <RowView>
+            {[1,2,3].map(item=><View
+            key={item}
+            from={{
+                translateY: -10,
+            }}
+            animate={{
+                translateY: 10,
+            }}
+            transition={{
+                loop: true,
+                type: 'spring',
+                duration: 100,
+                delay: item*100,
+            }}
+            style={styles.shape}
+            />)}
+        </RowView>
+    )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+    shape:{
+        padding:10,
+        borderRadius:100,
+        backgroundColor:color.active,
+        width:5,
+        margin:10
+    }
+})
---
+++ src/components/ServiceListView.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, View, Dimensions } from 'react-native'
+import moment from 'moment'
+
+import {Text, RowView} from 'styles'
+import color from 'colors'
+import TimeDiff from 'middlewares/TimeDiff'
+
+const WIDTH = Dimensions.get('screen').width
+
+const ServiceListView = () => {
+    const diff =TimeDiff(new Date(), new Date('2021-05-14'))
+
+    return (
+        <View style={[styles.container, {paddingBottom:10}]}>
+            <RowView style={{justifyContent: 'space-between',marginBottom:5}}>
+                <Text size={18} style={{width:WIDTH*.7}} bold numberOfLines={2} adjustsFontSizeToFit>Washing Machine Dryer not Working</Text>
+                <Text size={12}>{diff}</Text>
+            </RowView>
+            <Text>Burning Smell</Text>
+        </View>
+    )
+}
+
+export default ServiceListView
+
+const styles = StyleSheet.create({
+    container:{
+        borderBottomColor:color.lightDark,
+        borderBottomWidth:5,
+        marginBottom:10
+    }
+})
