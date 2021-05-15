ac3a883d4dc59c564f002566a3be6e43323f9f87
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-05-15 21:22:51 +0530

    first commit

---
+++ src/screen/profile/ServiceProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ServiceProfile = () => {
+    return (
+        <View>
+            <Text>Dhruv</Text>
+        </View>
+    )
+}
+
+export default ServiceProfile
+
+const styles = StyleSheet.create({})
