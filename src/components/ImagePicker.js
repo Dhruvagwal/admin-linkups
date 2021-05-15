732c35aad75dd9c5ff131de93f204cabcaf30e33
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-05-15 16:02:33 +0530

    first commit

---
+++ src/components/ImagePicker.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Platform, Pressable } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ImageSelector({style,image, setImage, children}) {
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [9, 16],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+    var list = [...image]
+    list.push(result.uri)
+    setImage(list)
+    }
+  };
+
+  return (
+    <Pressable onPress={pickImage} style={style}>
+        {children}
+    </Pressable>
+  );
+}
---
+++ src/components/ScreenModal.js
@@ -0,0 +1,38 @@
+import React, { Children, useRef, useEffect } from 'react'
+import {StyleSheet, View, Dimensions, Animated, StatusBar } from 'react-native'
+
+import Modal from 'react-native-modal';
+
+
+import color from 'colors'
+import {Text} from 'styles'
+
+const HEIGHT = Dimensions.get('screen').height
+const WIDTH = Dimensions.get('screen').width
+const ScreenModal = ({children, style}) => {
+    return (
+            <Modal backdropOpacity={0.5} isVisible animationIn='zoomIn'>
+                <StatusBar backgroundColor='rgba(0, 0, 0,0.5)' />
+                <View style={[styles.container, style]}>
+                    {children}
+                </View>
+            </Modal>
+    )
+}
+
+export default ScreenModal
+
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: color.elevatedDark,
+        flex: 1,
+        borderRadius:20,
+        padding:20,
+        elevation:5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1, 
+    }
+})
