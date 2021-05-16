ea7b1a317d13d236130d0d94dd07396735d12cdd
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-05-16 14:28:03 +0530

    first commit

--- src/components/NewFeedListView.js
+++ src/components/NewFeedListView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Dimensions, ScrollView, Pressable } from 'react-native'
+import { StyleSheet, View, Dimensions, ScrollView, Pressable, Image } from 'react-native'
 
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
 
@@ -20,20 +20,25 @@ const NewFeedListView = () => {
         <View style={styles.container}>
             <Pressable onPress={()=>RootNavgation.navigate(CONSTANT.OrderProfile)} style={{padding:10}}>
                 <RowView>
-                    <Text size={18} style={{width:WIDTH/1.4, marginRight:10}} numberOfLines={1} regular>Washing Machine Repair</Text>
-                    <Text size={13}>12 min ago</Text>
-                </RowView>
-                <RowView style={{justifyContent: 'space-evenly',marginTop:10}}>
-                    <MaterialCommunityIcons name="truck-delivery" size={ICON_SIZE} color={color.active} />
-                    <MaterialCommunityIcons name="clock-time-five" size={ICON_SIZE} color={color.active} />
-                    <RowView>
-                        <Text regular>5 </Text>
-                        <MaterialIcons name="local-offer" size={ICON_SIZE} color={color.active} />
-                    </RowView>
-                    <RowView>
-                        <Text regular>2.5m </Text>
-                        <MaterialCommunityIcons name="map-marker-distance" size={ICON_SIZE} color={color.active} />
-                    </RowView>
+                    <Image source={require('../../assets/washing-machine.png')} style={{width:80, height:80}} />
+                    <View style={{marginLeft:10}}>
+                        <RowView>
+                            <Text style={{width:WIDTH/2.2, marginRight:10}} numberOfLines={1} regular>Washing Machine Repair</Text>
+                            <Text size={13}>12 min ago</Text>
+                        </RowView>
+                        <RowView style={{justifyContent: 'space-between',marginTop:10}}>
+                            <MaterialCommunityIcons name="truck-delivery" size={ICON_SIZE} color={color.active} />
+                            <MaterialCommunityIcons name="clock-time-five" size={ICON_SIZE} color={color.active} />
+                            <RowView>
+                                <Text regular>5 </Text>
+                                <MaterialIcons name="local-offer" size={ICON_SIZE} color={color.active} />
+                            </RowView>
+                            <RowView>
+                                <Text regular>2.5m </Text>
+                                <MaterialCommunityIcons name="map-marker-distance" size={ICON_SIZE} color={color.active} />
+                            </RowView>
+                        </RowView>
+                    </View>
                 </RowView>
             </Pressable>
             <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{position: 'absolute', bottom:15}}>
--- src/screen/OrderProfile/index.js
+++ src/screen/OrderProfile/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StyleSheet, Dimensions, View } from 'react-native'
+import { StyleSheet, Dimensions, View, ScrollView, Image } from 'react-native'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'; 
 
 import {Text, RowView} from 'styles'
 import color from 'colors'
@@ -7,11 +8,69 @@ import color from 'colors'
 const HEIGHT = Dimensions.get('screen').height
 const WIDTH = Dimensions.get('screen').width
 
+const TopContainer = ()=><RowView style={styles.TopContainer}>
+    <Image source={require('../../../assets/washing-machine.png')} style={{height:'100%', width:'30%', resizeMode:'center'}}/>
+    <View style={{alignItems:'flex-start', justifyContent:'space-between', height: '85%',marginLeft:10}}>
+        <Text style={{width:'70%'}} size={18} regular>Washing Machine Repair</Text>
+        <Text size={13}>Posted 12 min ago</Text>
+    </View>
+</RowView> 
+
+const Point = ({children, text, top=false})=><RowView style={{...styles.Point, borderTopWidth : top?0:2}}>
+    {children}
+    <Text style={{marginLeft:10}}>{text}</Text>
+</RowView>
+
+const ICON_SIZE = 25
+const MiddleContainer = ()=><View style={{marginTop:20}}>
+    <Text style={{marginLeft:10, marginBottom:5}}>Details</Text>
+    <View style={styles.contentContainer}>
+        <Point top text='Timing: 3:00 Pm To 6:00 Am'>
+            <MaterialCommunityIcons name="clock-time-five" size={ICON_SIZE} color={color.active} />
+        </Point>
+        <Point text='5 Offers'>
+            <MaterialIcons name="local-offer" size={ICON_SIZE} color={color.active} />
+        </Point>
+        <Point text='Deleivery'>
+            <MaterialCommunityIcons name="truck-delivery" size={ICON_SIZE} color={color.active} />
+        </Point>
+        <Point text='2.5 Meters away from you'>
+            <MaterialCommunityIcons name="map-marker-distance" size={ICON_SIZE} color={color.active} />
+        </Point>
+    </View>
+</View>
+
+
+const Tag = ({text})=><View style={styles.tag}>
+    <Text regular>{text}</Text>
+</View> 
+
+const Category = ()=><View style={{marginTop:20}}>
+    <Text style={{marginLeft:10, marginBottom:5}}>Tags</Text>
+    <View style={styles.contentContainer}>
+        <RowView >
+            <Tag text='Electrician' />
+            <Tag text='Washing Machine' />
+        </RowView>
+        <RowView style={{marginTop:10}}>
+            <Tag text='Smoking' />
+            <Tag text='Short Crcuit' />
+        </RowView>
+    </View>
+</View>
+
 const Index = () => {
     return (
-        <View style={{flex:1, paddingTop:HEIGHT*.05, padding: 20,}}>
-            <View>
-                <Text>Check</Text>
+        <View style={{flex:1, paddingTop:HEIGHT*.1, padding: 10,}}>
+            <ScrollView>
+                <TopContainer/>
+                <MiddleContainer/>
+                <Category/>
+                <Text>{'\n'}</Text>
+                <Text>{'\n'}</Text>
+            </ScrollView>
+            <View style={styles.bottomButton}>
+                <Text regular>Accept</Text>
             </View>
         </View>
     )
@@ -19,4 +78,39 @@ const Index = () => {
 
 export default Index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    bottomButton:{
+        backgroundColor:color.active,
+        padding: 20,
+        position: 'absolute',
+        bottom:0,
+        width:WIDTH,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    TopContainer:{
+        backgroundColor:color.elevatedDark,
+        borderRadius:10,
+        overflow: 'hidden',
+        height:HEIGHT*.15,
+        padding: 10,
+    },
+    Point:{
+        borderTopWidth:2,
+        borderTopColor:color.lightDark,
+        paddingVertical:10
+    },
+    contentContainer:{
+        backgroundColor:color.elevatedDark, 
+        padding: 10,
+        borderRadius:10
+    },
+    tag:{
+        backgroundColor:color.lightDark,
+        padding:5,
+        marginHorizontal:5,
+        borderRadius:10,
+        flexGrow:1,
+        alignItems:'center'
+    }
+})
