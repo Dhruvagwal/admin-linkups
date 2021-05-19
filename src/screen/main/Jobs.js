d84b484a2dcd9914c5187e0fb040807c815a7c1c
Dhruv Aggarwal <dhruvagwal.clus@gmail.com>
2021-05-19 14:55:37 +0530

    UI_Commit

---
+++ src/screen/main/Jobs.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { StyleSheet, View, Dimensions, ScrollView, Pressable, RefreshControl } from 'react-native'
+import NewFeedListView from 'components/NewFeedListView'
+import { MaterialIcons } from '@expo/vector-icons'; 
+import {Text, RowView} from 'styles'
+import color from 'colors'
+
+const HEIGHT = Dimensions.get('screen').height
+const WIDTH = Dimensions.get('screen').width
+
+const Jobs = ({newOrder, category, loadData}) => {
+    const [refreshing, setRefreshing] = React.useState(false);
+    const refresh = async ()=>{
+        setRefreshing(true)
+        await loadData()
+        setRefreshing(false)
+    }
+
+    return (
+        <>        
+            <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={refresh}
+                        color={[color.active]}
+                    />
+                }
+            >
+                <View style={{flex:1, backgroundColor:color.lightDark}}>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        {
+                            newOrder.map(item=><NewFeedListView key={item.id} data={item} category={category}/>)
+                        }
+                        <Text>{'\n'}</Text>
+                    </ScrollView>
+                </View>
+            </ScrollView>
+            <View style={styles.filter}>
+                <MaterialIcons name="filter-alt" size={40} color={color.white} />
+            </View>
+        </>
+    )
+}
+
+export default Jobs
+
+const styles = StyleSheet.create({
+    filter:{
+        position: 'absolute',
+        bottom:20,
+        right:20,
+        padding:10,
+        backgroundColor:color.blue,
+        borderRadius:100,
+        opacity: 0.95,
+    },
+    loading:{
+        position: 'absolute',
+        width:200,
+        bottom:-22,
+        alignSelf:'center'
+    }
+})
---
+++ src/screen/main/Library.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import { StyleSheet, View, ScrollView, Pressable, RefreshControl,Dimensions} from 'react-native'
+import {Text, RowView} from 'styles'
+import NewFeedListView from 'components/NewFeedListView'
+import color from 'colors'
+
+const HEIGHT = Dimensions.get('screen').height
+const WIDTH = Dimensions.get('screen').width
+
+const list = ['Posted', 'Invited', 'Inprogress', 'Completed']
+
+const Library = ({state, proposed, category, loadData, invited})=>{
+    const [active, setActive] = useState(list[0])
+    const [data, setData] = useState(proposed)
+    const [refreshing, setRefreshing] = useState(false);
+
+    const refresh = async ()=>{
+        setRefreshing(true)
+        active === list[0] && await loadData()
+        setRefreshing(false)
+    }
+
+    const getData = async (active)=>{
+        setRefreshing(true)
+        setActive(active)
+        active === list[0] && setData(proposed)
+        active === list[1] && setData(invited)
+        active === list[2] && setData([])
+        setRefreshing(false)
+    }
+
+    return <View style={{flex:1}}>
+        <ScrollView
+            refreshControl={
+                <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={refresh}
+                    color={[color.active]}
+                />
+            }
+        >
+            {
+                data.map(item=><NewFeedListView key={item.id} data={item} category={category} invited={active!==list[1]}/>)
+            }
+        </ScrollView>
+        <ScrollView horizontal style={styles.bottomBar}>
+                {
+                    list.map((item)=><Pressable onPress={()=>getData(item)} key={item} style={[styles.text, active===item && styles.textActive]}>
+                            <Text size={15}>{item}</Text>
+                        </Pressable>)
+                }
+        </ScrollView>
+    </View>
+}
+
+export default Library
+
+const styles = StyleSheet.create({
+    bottomBar:{
+        backgroundColor:color.dark,
+        position: 'absolute',
+        bottom:0,
+        // width:WIDTH/2.8*list.length
+    },
+    text:{
+        width:WIDTH/2.5,
+        padding:20,
+        alignItems:'center',
+    },
+    textActive:{
+        borderTopColor:color.active,
+        borderTopWidth:4,
+    }
+})
